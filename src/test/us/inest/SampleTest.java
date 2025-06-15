00b0a6bcbdd6a135ffdf4ce78bdb00deff44a16f
inest-us <hieung@live.com>
2025-06-15 16:31:15 -0500

    setup junit
    setup junit

--- src/test/us/inest/SampleTest.java
+++ src/test/us/inest/SampleTest.java
@@ -1,4 +1,12 @@
 package us.inest;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class SampleTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
 }
