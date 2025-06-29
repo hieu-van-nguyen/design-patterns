8ecc651dc73cea80c06ba2a30070814eff961752
inest-us <hieung@live.com>
2025-06-29 11:50:12 -0500

    update unit test version
    update unit test version

--- src/test/us/inest/SampleTest.java
+++ src/test/us/inest/SampleTest.java
@@ -1,12 +1,33 @@
 package us.inest;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SampleTest {
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("Before Class");
+    }
+
+    @BeforeEach
+    public void before() {
+        System.out.println("Before Each Test Case");
+    }
+
     @Test
     public void sampleTest() {
         assertTrue(true);
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("After Class");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("After Each Test Case");
     }
 }
